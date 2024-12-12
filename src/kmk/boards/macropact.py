beb82573ac11cb07a9823abaed5cf4e240234395
Gareth Faulkner <parriehunter@hotmail.com>
2024-12-12 13:34:55 +1300

    Update macropact.py
    Fixed coord_mapping for 3 x 8 matrix

--- src/kmk/boards/macropact.py
+++ src/kmk/boards/macropact.py
@@ -10,11 +10,10 @@ class KMKKeyboard(_KMKKeyboard):
     column_pins=(board.GP3, board.GP4, board.GP5, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10),
     diode_orientation = DiodeOrientation.COLUMNS
     coord_mapping = []
-    coord_mapping.extend(ic(0, x) for x in range(5))
-    coord_mapping.extend(ic(1, x) for x in range(5))
-    coord_mapping.extend(ic(2, x) for x in range(5))
-    coord_mapping.extend(ic(3, x) for x in range(5))
-    
+    coord_mapping.extend(ic(0, x) for x in range(8))
+    coord_mapping.extend(ic(1, x) for x in range(8))
+    coord_mapping.extend(ic(2, x) for x in range(8))
+
     
     
     
