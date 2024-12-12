2158c3d8623aef795165b72cace4789e12ef0cd0
Gareth Faulkner <parriehunter@hotmail.com>
2024-12-12 13:17:53 +1300

    Update macropact.py
    Changed to a 3x8 matrix to simplify wiring and coding

--- src/kmk/boards/macropact.py
+++ src/kmk/boards/macropact.py
@@ -6,8 +6,8 @@ from kmk.matrix import intify_coordinate as ic
 
 
 class KMKKeyboard(_KMKKeyboard):
-    row_pins=(board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5),
-    column_pins=(board.GP6, board.GP7, board.GP8, board.GP9),
+    row_pins=(board.GP0, board.GP1, board.GP2),
+    column_pins=(board.GP3, board.GP4, board.GP5, board.GP6, board.GP7, board.GP8, board.GP9, board.GP10),
     diode_orientation = DiodeOrientation.COLUMNS
     coord_mapping = []
     coord_mapping.extend(ic(0, x) for x in range(5))
